1b720c53216177d72e4d575eaabd52c1020d5277
Zeuva <zeuvastec@gmail.com>
2026-08-26 16:57:11 -0300

    Delete health.js

--- health.js
+++
@@ -1,3 +0,0 @@
-module.exports = function handler(req, res) {
-  res.status(200).json({ ok: true, openaiKeyConfigured: !!process.env.OPENAI_API_KEY, model: process.env.OPENAI_MODEL || 'gpt-5.4-mini' });
-};
